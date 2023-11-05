7ce7029307051178b344a7d749e1145cf09e3be6
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-11-05 23:22:36 +0530

    Memoize

---
+++ Function Transformations/memoize.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache = {};
+    return function(...args) {
+        let key = [...args]
+        if(cache[key] === undefined){
+            return cache[key] = fn(...args);
+        }
+        return cache[key];
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
