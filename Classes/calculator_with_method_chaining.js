8995e68eb136c7cc78ee5a8749fe1651e7e364e5
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-09-26 14:49:22 +0530

    Method chaining

--- Classes/calculator_with_method_chaining.js
+++ Classes/calculator_with_method_chaining.js
@@ -1,3 +1,8 @@
+/*
+Method chaining is a programming technique where multiple methods are called on the same object consecutively in a single statement. Each method returns the object itself (usually this), allowing the next method in the chain to be called on the same object.
+*/
+
+
 class Calculator {
 
     /** 
